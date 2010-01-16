c68d0b16b334c4c6c944f30262058d7ef05f741d
David Wolinsky <davidiw@ufl.edu>
2010-01-16 01:26:42 -0500

    Abstract changes and changes to title to reflect paper topic not conference

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
-latex hpdc10 -interaction=nonstopmode
-bibtex hpdc10
-latex hpdc10 -interaction=nonstopmode
-latex hpdc10 -interaction=nonstopmode
-dvips -t letter hpdc10.dvi
-ps2pdf hpdc10.ps
-xpdf hpdc10.pdf
+latex DecentralizedGrids -interaction=nonstopmode
+bibtex DecentralizedGrids
+latex DecentralizedGrids -interaction=nonstopmode
+latex DecentralizedGrids -interaction=nonstopmode
+dvips -t letter DecentralizedGrids.dvi
+ps2pdf DecentralizedGrids.ps
+xpdf DecentralizedGrids.pdf
